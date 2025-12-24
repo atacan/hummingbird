3172405991470c278a465a0ef0f1c423fd65a610
atacan <atacandur@icloud.com>
2025-12-24 19:12:52 +0100

    more HTTP:URI: benchmarks

--- Benchmarks/HummingbirdBenchmarks/HTTPBenchmarks.swift
+++ Benchmarks/HummingbirdBenchmarks/HTTPBenchmarks.swift
@@ -17,6 +17,8 @@ import Benchmark
 import HummingbirdCore
 
 func httpBenchmarks() {
+    // MARK: - URI Parsing Benchmarks
+
     Benchmark("HTTP:URI:Decode", configuration: .init(scalingFactor: .kilo)) { benchmark in
         benchmark.startMeasurement()
         for _ in benchmark.scaledIterations {
@@ -24,12 +26,91 @@ func httpBenchmarks() {
         }
     }
 
+    Benchmark("HTTP:URI:Decode:PathOnly", configuration: .init(scalingFactor: .kilo)) { benchmark in
+        benchmark.startMeasurement()
+        for _ in benchmark.scaledIterations {
+            blackHole(URI("/test/this/path"))
+        }
+    }
+
+    Benchmark("HTTP:URI:Decode:FullURL", configuration: .init(scalingFactor: .kilo)) { benchmark in
+        benchmark.startMeasurement()
+        for _ in benchmark.scaledIterations {
+            blackHole(URI("https://example.com:8080/test/path?query=value#fragment"))
+        }
+    }
+
+    Benchmark("HTTP:URI:Decode:LongPath", configuration: .init(scalingFactor: .kilo)) { benchmark in
+        benchmark.startMeasurement()
+        for _ in benchmark.scaledIterations {
+            blackHole(URI("/api/v1/users/12345/posts/67890/comments/11111/replies"))
+        }
+    }
+
+    // MARK: - URI Property Access Benchmarks
+
     Benchmark("HTTP:URI:QueryParameters", configuration: .init(scalingFactor: .kilo)) { benchmark in
         benchmark.startMeasurement()
         for _ in benchmark.scaledIterations {
             blackHole(URI("/test?this=true&that=false&percent=%45this%48").queryParameters)
         }
     }
+
+    Benchmark("HTTP:URI:QueryParameters:Many", configuration: .init(scalingFactor: .kilo)) { benchmark in
+        benchmark.startMeasurement()
+        for _ in benchmark.scaledIterations {
+            blackHole(URI("/test?a=1&b=2&c=3&d=4&e=5&f=6&g=7&h=8&i=9&j=10").queryParameters)
+        }
+    }
+
+    Benchmark("HTTP:URI:QueryParameters:NoPercentEncoding", configuration: .init(scalingFactor: .kilo)) { benchmark in
+        benchmark.startMeasurement()
+        for _ in benchmark.scaledIterations {
+            blackHole(URI("/test?this=true&that=false&other=value").queryParameters)
+        }
+    }
+
+    Benchmark("HTTP:URI:Path:Access", configuration: .init(scalingFactor: .kilo)) { benchmark in
+        let uri = URI("/test/this/path?query=value")
+        benchmark.startMeasurement()
+        for _ in benchmark.scaledIterations {
+            blackHole(uri.path)
+        }
+    }
+
+    Benchmark("HTTP:URI:Query:Access", configuration: .init(scalingFactor: .kilo)) { benchmark in
+        let uri = URI("/test?this=true&that=false")
+        benchmark.startMeasurement()
+        for _ in benchmark.scaledIterations {
+            blackHole(uri.query)
+        }
+    }
+
+    Benchmark("HTTP:URI:Host:Access", configuration: .init(scalingFactor: .kilo)) { benchmark in
+        let uri = URI("https://example.com:8080/test/path")
+        benchmark.startMeasurement()
+        for _ in benchmark.scaledIterations {
+            blackHole(uri.host)
+        }
+    }
+
+    Benchmark("HTTP:URI:Scheme:Access", configuration: .init(scalingFactor: .kilo)) { benchmark in
+        let uri = URI("https://example.com/test")
+        benchmark.startMeasurement()
+        for _ in benchmark.scaledIterations {
+            blackHole(uri.scheme)
+        }
+    }
+
+    Benchmark("HTTP:URI:Port:Access", configuration: .init(scalingFactor: .kilo)) { benchmark in
+        let uri = URI("https://example.com:8080/test")
+        benchmark.startMeasurement()
+        for _ in benchmark.scaledIterations {
+            blackHole(uri.port)
+        }
+    }
+
+    // MARK: - Cookie Benchmarks
 
     Benchmark("HTTP:Cookie:Decode", configuration: .init(scalingFactor: .kilo)) { benchmark in
         benchmark.startMeasurement()
